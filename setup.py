aa79bc7637b7c644157f4b25de5c447b939f023d
Andrei Radulescu-Banu <bitdribble@gmail.com>
2018-02-16 00:34:21 -0500

    devel

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
+import sys, os
 from cx_Freeze import setup, Executable
 
 base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+    os.environ['TCL_LIBRARY'] = "C:\\Users\\andrei\\AppData\\Local\\Programs\\Python\\Python36\\DLLs"
+    os.environ['TK_LIBRARY'] = "C:\\Users\\andrei\\AppData\\Local\\Programs\\Python\\Python36\\DLLs"
 
-executables = [Executable("wxPortfolio.py", base=base)]
+executables = [
+    Executable('wxPortfolio.py', base=base)
+]
 
-packages = ["idna"]
-options = {
-    'build_exe': {
-        'packages':packages,
-    },
-}
+# Dependencies are automatically detected, but might need fine tuning.
+build_exe_options = {'packages': ['pandas', 'numpy'],
+                     }
 
-setup(
-    name = "<any name>",
-    options = options,
-    version = "<any number>",
-    description = '<any description>',
-    executables = executables
+setup(name='wxPortfolio',
+      version='0.1',
+      description='Portfolio management software',
+      executables=executables
 )
