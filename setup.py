cccb4dfc1105afb4e82281bd8805ca09d5dc6313
Andrei Radulescu-Banu <bitdribble@gmail.com>
2018-02-16 23:12:22 -0500

    devel

--- setup.py
+++ setup.py
@@ -12,11 +12,14 @@ executables = [
 ]
 
 # Dependencies are automatically detected, but might need fine tuning.
-build_exe_options = {'packages': ['pandas', 'numpy'],
-                     }
+build_exe_options = {
+    'packages': ['pandas', 'numpy'],
+    'includes': ['pandas', 'numpy']
+    }
 
 setup(name='wxPortfolio',
       version='0.1',
       description='Portfolio management software',
+      options = {"build_exe" : build_exe_options},
       executables=executables
 )
