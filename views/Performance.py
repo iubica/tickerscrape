121d68ac64987f024f4baf52891b5885c2b36af9
Andrei Radulescu-Banu <bitdribble@gmail.com>
2018-03-19 23:26:01 -0400

    devel

--- views/Performance.py
+++ views/Performance.py
@@ -19,42 +19,64 @@ def GetWindow(frame, nb, log):
     if Config.holdingsDf is None:
         Config.GetHoldings()
 
+    # Make an account list
+    accountList = list()
+
+    for i in range(Config.accountsDf.shape[0]):
+        account = Config.accountsDf.ix[i, "Account Name"]
+        
+        if account in accountList:
+            continue
+
+        accountList.append(account)
+
     # Make list of per-account tickers
     tickerList = list()
 
     # For each ticker in the config...
     for i in range(Config.holdingsDf.shape[0]):
         ticker = Config.holdingsDf.ix[i, "Ticker"]
-        account = Config.holdingsDf.ix[i, "Account"]
-        
-        if (ticker, account) in tickerList:
+
+        if ticker in tickerList:
             continue
 
-        tickerList.append((ticker, account))
+        tickerList.append(ticker)
 
     # Create a performance dataframe
-    pf = pd.DataFrame(index = np.arange(0, len(tickerList)),
-                      columns = ['Ticker', 'Account', 'Last Price', 'Shares', 'Current Value', 'Cost Basis'])
+    pf = pd.DataFrame(index = np.arange(0, len(accountList) + len(tickerList)),
+                      columns = ["Ticker", "Last Price", "Shares", "Current Value", "Cost Basis"])
 
     row = 0
-    for (ticker, account) in tickerList:
-        pf.ix[row, "Ticker"] = ticker
-        pf.ix[row+1, "Ticker"] = " " + morningstar.ticker_name(ticker.upper())
-        pf.ix[row, "Account"] = account
-        pf.ix[row+1, "Account"] = ""
-
-        # Compute the shares
-        shares = 0
-        sharesSet = True
-        costBasis = 0
-        costBasisSet = True
+    for account in accountList:
+        pf.ix[row, "Ticker"] = account
+        pf.ix[row, "Last Price"] = ""
+        pf.ix[row, "Shares"] = ""
+        pf.ix[row, "Current Value"] = ""
+        pf.ix[row, "Cost Basis"] = ""
+        row = row+1
         for i in range(Config.holdingsDf.shape[0]):
-            ticker1 = Config.holdingsDf.ix[i, "Ticker"]
-            account1 = Config.holdingsDf.ix[i, "Account"]
-            shares1 = Config.holdingsDf.ix[i, "Shares"]
-            costBasis1 = Config.holdingsDf.ix[i, "Cost Basis"]
-            if ticker == ticker1 and account == account1:
-                if shares1:                    
+            if Config.holdingsDf.ix[i, "Account"] != account:
+                continue
+            
+            pf.ix[row, "Ticker"] = ticker
+            pf.ix[row+1, "Ticker"] = " " + morningstar.ticker_name(ticker.upper())
+
+            # Compute the shares
+            shares = 0
+            sharesSet = True
+            costBasis = 0
+            costBasisSet = True
+            
+            for j in range(Config.holdingsDf.shape[0]):
+                if Config.holdingsDf.ix[j, "Ticker"] != ticker:
+                    continue
+                if Config.holdingsDf.ix[j, "Account"] != account:
+                    continue
+
+                shares1 = Config.holdingsDf.ix[j, "Shares"]
+                costBasis1 = Config.holdingsDf.ix[j, "Cost Basis"]
+
+                if shares1:
                     shares = shares + Format.StringToFloat(shares1)
                 else:
                     sharesSet = False
@@ -63,25 +85,25 @@ def GetWindow(frame, nb, log):
                 else:
                     costBasisSet = False
 
-        if sharesSet:
-            pf.ix[row, "Shares"] = Format.FloatToString(shares, 3)
-        else:
-            pf.ix[row, "Shares"] = ""
-        pf.ix[row+1, "Shares"] = ""
+            if sharesSet:
+                pf.ix[row, "Shares"] = Format.FloatToString(shares, 3)
+            else:
+                pf.ix[row, "Shares"] = ""
+            pf.ix[row+1, "Shares"] = ""
             
-        pf.ix[row, "Last Price"] = ""
-        pf.ix[row+1, "Last Price"] = ""
+            pf.ix[row, "Last Price"] = ""
+            pf.ix[row+1, "Last Price"] = ""
 
-        pf.ix[row, "Current Value"] = ""
-        pf.ix[row+1, "Current Value"] = ""
+            pf.ix[row, "Current Value"] = ""
+            pf.ix[row+1, "Current Value"] = ""
 
-        if sharesSet and costBasisSet:
-            pf.ix[row, "Cost Basis"] = "$" + Format.FloatToString(costBasis/shares, 2) + "/Share"
-            pf.ix[row+1, "Cost Basis"] = " $" + Format.FloatToString(costBasis, 2)
-        else:
-            pf.ix[row, "Cost Basis"] = ""
+            if sharesSet and costBasisSet:
+                pf.ix[row, "Cost Basis"] = "$" + Format.FloatToString(costBasis/shares, 2) + "/Share"
+                pf.ix[row+1, "Cost Basis"] = " $" + Format.FloatToString(costBasis, 2)
+            else:
+                pf.ix[row, "Cost Basis"] = ""
             
-        row = row+2
+            row = row+2
     
     # Promote 1st column as new index
     pf2 = pf.set_index("Ticker")
